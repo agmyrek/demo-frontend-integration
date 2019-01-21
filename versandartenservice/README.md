a9cfc753ca7a084ed27892801b57977f825b6cdd
Akademix <andreasgmyrek@gmx.de>
2019-01-21 21:11:27 +0100

    Dependencies und ersten Controller eingefügt

---
+++ versandartenservice/README.md
@@ -0,0 +1,13 @@
+# Projekt bauen und starten
+
+* Über Maven projekt bauen:
+
+maven clean install
+
+* JAR im target-Verzeichnis ausführen
+
+java -jar versandartenservice-0.0.1-SNAPSHOT.jar
+
+## Ressourcen
+
+* localhost:8080/versandarten
--- versandartenservice/src/main/java/de/angmyrek/versandartenservice/ui/VersandartenController.java
+++ versandartenservice/src/main/java/de/angmyrek/versandartenservice/ui/VersandartenController.java
@@ -1,5 +1,14 @@
 package de.angmyrek.versandartenservice.ui;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class VersandartenController {
+
+    @GetMapping("/versandarten")
+    public String versandartenAuswahl(){
+        return "Standard, Premium, Filialabholung";
+    }
 }
