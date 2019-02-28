ef0608d2449b87c43806d40cb4d8de866f1d0042
Akademix <andreasgmyrek@gmx.de>
2019-02-28 21:50:10 +0100

    Kaufen Button Eventhandler

--- checkoutservice/src/main/resources/static/js/checkout.js
+++ checkoutservice/src/main/resources/static/js/checkout.js
@@ -1,19 +1,34 @@
 
-
+var checkoutData = {
+        artikelId : '',
+        zahlungsart : ''
+};
 
 window.onload = function () {
 
     includeZahlungsartenauswahl();
-    registerEventhandler();
+    addZahlungsartAusgewaehltEventHandler();
+    addKaufenEventHandler();
 };
 
 function includeZahlungsartenauswahl(){
     $( "#zahlungsarten-checkout-content" ).load( "http://localhost:8081/zahlungsarten" );
 }
 
-function registerEventhandler(){
+function addZahlungsartAusgewaehltEventHandler(){
 
     window.addEventListener('zahlungsart-ausgewaehlt', function (event) {
         console.log("Event: zahlungsart-ausgewaehlt event im Checkoutservice erhalten: "+event.detail);
+        checkoutData.zahlungsart = event.detail;
+
     }, false);
+}
+
+function addKaufenEventHandler(){
+
+    document.getElementById("kaufen-button").addEventListener("click", function(event){
+
+        console.log("Kaufen: "+JSON.stringify(checkoutData));
+    });
+
 }
